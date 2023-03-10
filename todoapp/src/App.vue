f79b6460f8e5e58af2cc4f93514a9ba0a13bc6c1
dominik@ffdk.academy <dominik@ffdk.academy>
2023-03-10 12:11:19 +0100

    added tailwind

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="app">
-    <ToDo msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">Unsere ToDos</h1>
+      <h1 class="text-xl">Offene ToDos:{{ todos.length }}</h1>
+    </div>
+    <ToDo/>
   </div>
 </template>
 
@@ -9,6 +13,13 @@ import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      todos:[
+        {todo: "Einkaufen", done: false},
+      ],
+    };
+  },
   components: {
     ToDo
   }
